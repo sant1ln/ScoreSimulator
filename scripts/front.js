605928a1fc9a0df639f8ce8a536af378dbe29c73
Santiago Lopera <santiagoln09@gmail.com>
2020-06-28 00:15:07 -0500

    Frontend End

---
+++ scripts/front.js
@@ -0,0 +1,30 @@
+const tabla = document.querySelector('table')
+const contenedor = document.querySelector('#bodyelements');
+const contBoton = document.querySelector('#buttonCont');
+
+document.querySelector('#cantidad').addEventListener('change',(e)=>{
+    console.log(e.target.value);
+    
+
+    
+     for(let i=0;i<e.target.value;i++){
+         console.log(i)
+         var cont = document.createElement("tr");
+         var td1 = document.createElement("td");
+         var td2 = document.createElement("td");
+         var inpt = document.createElement("input")
+         var inpt2 = document.createElement("input")
+         inpt.classList.add('note')
+         inpt2.classList.add('note')
+         td1.appendChild(inpt);
+         td2.appendChild(inpt2);
+         cont.appendChild(td1);
+         cont.appendChild(td2);
+         contenedor.appendChild(cont)
+    }
+    let button = document.createElement('button');
+    let contenido = document.createTextNode("Calcular")
+    button.appendChild(contenido)
+    contBoton.appendChild(button)
+   
+})
